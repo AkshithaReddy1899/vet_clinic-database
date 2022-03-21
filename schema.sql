4c0ee5648a460d66c47752adc3e5f3a2f7c9f334
AkshithaReddy1899 <akshithareddy1899@gmail.com>
2022-03-21 20:37:39 +0530

    Fill the files with commands

--- schema.sql
+++ schema.sql
@@ -1,5 +1,20 @@
 /* Database schema to keep the structure of entire database. */
 
+/* Create database */
+
+CREATE DATABASE vet_clinic;
+
+/* Connect to database */
+
+\c vet_clinic
+
+/* create table */
+
 CREATE TABLE animals (
-    name varchar(100)
+  id INT,
+  name VARCHAR(150),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg FLOAT
 );
